4281edd7eac62a2a897a7ce3473820e564424993
sivepanda <sivenpanda@icloud.com>
2026-04-17 13:29:23 -0400

    update xpsl

--- init.lua
+++ init.lua
@@ -538,6 +538,22 @@ require("lazy").setup({
 					end,
 				},
 			})
+			local lspconfig = require("lspconfig")
+			local configs = require("lspconfig.configs")
+
+			if not configs.carya then
+			  configs.carya = {
+				default_config = {
+				  cmd = { "carya" },
+				  filetypes = { "carya" },
+				  root_dir = lspconfig.util.root_pattern(".git"),
+				},
+			  }
+			end
+
+			lspconfig.carya.setup({
+			  capabilities = capabilities,
+			})
 		end,
 	},
 
@@ -755,52 +771,25 @@ require("lazy").setup({
 	},
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"jsdoc",
-				"bash",
-				"c",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"python",
-				"vim",
-				"vimdoc",
-				"go",
-				"gomod",
-				"gowork",
-				"gosum",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
-		config = function(_, opts)
-			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+		config = function()
+			require("nvim-treesitter").setup({
+				ensure_installed = {
+					"javascript", "typescript", "jsdoc", "bash", "c",
+					"html", "lua", "luadoc", "markdown", "python",
+					"vim", "vimdoc", "go", "gomod", "gowork", "gosum",
+				},
+				auto_install = true,
+			})
 
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
-
-			-- There are additional nvim-treesitter modules that you can use to interact
-			-- with nvim-treesitter. You should go explore a few and see what interests you:
-			--
-			--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-			--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
+			})
 		end,
 	},
-
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
